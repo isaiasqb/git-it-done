19cf7144216cf2d5bc4af236d85b14ecd0167161
IsaiasQB <isaias.qb@hotmail.com>
2022-03-27 00:41:10 -0400

    can now search user repos by name

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,5 +9,25 @@ var getUserRepos = function (user){
         });
     });
 };        
-getUserRepos("isaiasqb")
 
+
+var userFormEl = document.querySelector("#user-form");
+var nameInput = document.querySelector("#username");
+
+    //to be executed upon a form submission browser event
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+        //get the name of the user form the input value
+    var username = nameInput.value.trim();
+
+    if(username){
+        getUserRepos(username); //if there is a username, it runs the function to fetch data using that username as parameter
+        nameInput.value = ""    //resets the value of the input area
+    } else {
+        alert("Please enter a GitHub username")
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
